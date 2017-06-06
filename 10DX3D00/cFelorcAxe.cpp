e9ccfde729f1099199de92e803677e15da2709d7
HongJunYeong <red_connec@naver.com>
2017-06-06 18:07:29 +0900

    cFelorcAxe 클래스 추가

---
+++ 10DX3D00/cFelorcAxe.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include "cFelorcAxe.h"
+
+
+cFelorcAxe::cFelorcAxe()
+{
+}
+
+
+cFelorcAxe::~cFelorcAxe()
+{
+}
+
+void cFelorcAxe::Setup()
+{
+}
+
+void cFelorcAxe::Update()
+{
+}
+
+void cFelorcAxe::Render()
+{
+}
---
+++ 10DX3D00/cFelorcAxe.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "cMonster.h"
+
+class cFelorcAxe : public cMonster
+{
+public:
+	cFelorcAxe();
+	virtual ~cFelorcAxe();
+
+public:
+	virtual void Setup() override;
+	virtual void Update() override;
+	virtual void Render() override;
+};
+
