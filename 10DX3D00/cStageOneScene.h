ff36dccedf782a91d15371f851927484490795e5
HongJunYeong <red_connec@naver.com>
2017-06-09 11:46:38 +0900

    cStageOneScene 수정

--- 10DX3D00/cStageOneScene.h
+++ 10DX3D00/cStageOneScene.h
@@ -1,11 +1,21 @@
 #pragma once
 #include "cScene.h"
 
+class cMtlTex;
+class xFileToMesh;
+
 class cStageOneScene : public cScene
 {
 public:
 	cStageOneScene();
 	virtual ~cStageOneScene();
+
+private:
+	vector<ST_OBJECT*>				m_vecObj;
+	ST_OBJECT_INFO*					m_pStObject; // 공용헤더에 선언된 구조체 포인터
+	map<string, ST_OBJECT_INFO*>    m_mapObj; // 스트링으로 관리할 예정
+	LPD3DXMESH						m_pObjMesh;
+	vector<cMtlTex*>				m_vecObjMtlTex;
 
 public:
 	virtual void Setup() override;
@@ -13,5 +23,9 @@ public:
 	virtual void Render(LPD3DXSPRITE pSprite) override;
 	virtual void Release() override;
 	virtual void StartRecvThread() override;
+
+	//
+	void ObjSetup();
+	void ObjRender();
 };
 
