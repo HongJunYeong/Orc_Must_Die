68fc3ce08dc3afc0ab5e92f32912c0f2914e6bd7
HongJunYeong <red_connec@naver.com>
2017-06-12 15:40:24 +0900

    skinned매니저 구현

---
+++ 10DX3D00/cSkinnedMeshManager.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define g_pSkinnedMeshManager cSkinnedMeshManager::GetInstance()
+
+class cSkinnedMesh;
+
+class cSkinnedMeshManager
+{
+private:
+	std::map<std::string, cSkinnedMesh*> m_mapSkinnedMesh;
+
+public:
+	SINGLETONE(cSkinnedMeshManager);
+
+	cSkinnedMesh* GetSkinnedMesh(char* szFolder, char* szFilename);
+	void AddSkinnedMesh(char* szFolder, char* szFileName);
+	
+	void Destroy();
+};
+
