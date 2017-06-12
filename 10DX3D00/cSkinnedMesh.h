c90d7bb70d3ab18f4d821fd41ae54bf73ce18fac
HongJunYeong <red_connec@naver.com>
2017-06-12 15:37:50 +0900

    멀티애니메이션 구현

--- 10DX3D00/cSkinnedMesh.h
+++ 10DX3D00/cSkinnedMesh.h
@@ -1,42 +1,64 @@
 #pragma once
+
+struct ST_BONE;
+
 class cSkinnedMesh
 {
-public:
-	cSkinnedMesh();
-	~cSkinnedMesh();
+	//friend class cSkinnedMeshManager;
 
-protected:
-	LPD3DXFRAME m_pRoot;
+private:
+	//하나만 생성
+	ST_BONE*					m_pRootFrame;			
+	DWORD						m_dwWorkingPaletteSize;
+	D3DXMATRIX*					m_pmWorkingPalette;
+	LPD3DXEFFECT				m_pEffect;
+	ST_SPHERE					m_stBoundingSphere;
+
+	// 객체마다 생성
 	LPD3DXANIMATIONCONTROLLER	m_pAnimController;
+	D3DXVECTOR3					m_vPosition;
 
-	//	>>:
-
-	float	m_fBlendTime;
-	float	m_fPassedBlendTime;
-	bool	m_isAnimBlend;
-	bool	m_bPlay;		//애니메이션이 플레이중인가??	
-	bool	m_bLoop;		//애니메이션이 루프 플레이중인가??					
-	//	<<:
+	float m_fBlendTime;
+	float m_fPassedBlendTime;
+	bool  m_isAnimBlend;
 public:
-	void Setup(char* szFolder, char* szFile);
-	void Update();
-	void Update(LPD3DXFRAME pFrame, LPD3DXFRAME pParent);
-	void Render(LPD3DXFRAME pFrame, D3DXMATRIXA16* matWorld);
-	void SetupBoneMatrixPtrs(LPD3DXFRAME pFrame);
-	void UpdateSkinnedMesh(LPD3DXFRAME pFrame);
-
+	cSkinnedMesh(char* szFolder, char* szFilename);
+	cSkinnedMesh();
+	~cSkinnedMesh(void);
+	
+	void UpdateAndRender(D3DXMATRIXA16& mat);
 	void SetAnimationIndex(int nIndex);
 	void SetAnimationIndexBlend(int nIndex);
 
-	LPD3DXFRAME GetRootFrame();
-	void SetRootFrame(LPD3DXFRAME pFrame);
+	void SetRandomTrackPosition(); // 테스트용
+	void SetPosition(D3DXVECTOR3 v)
+	{
+		m_vPosition = v;
+		m_stBoundingSphere.vCenter = v;
+	}
+	ST_SPHERE* GetBoundingSphere()
+	{
+		return &m_stBoundingSphere;
+	}
 
-	double GetAniTime(int nindex);//인덱스 위치의 애니메이션 길이를 가져옴
+	ST_BONE* GetRootFrame();
+	void SetRootFrame(ST_BONE* pFrame);
+
+	double GetAniTime(int nIndex);//인덱스 위치의 애니메이션 길이를 가져옴
 	double GetAniTrackPosition();//현재 진행중인 애니메이션의 위치를 가져옴
 
-	//인덱스 위치의 애니메이션 종료시 선택한 애니메이션으로 변환
-	//nIndex = 현재 사용중인 애니메이션 셋
-	//nOutIndex = 현재 사용중인 애니메이션 종료시 변경하고자 하는 애니메이션 셋
+								 //인덱스 위치의 애니메이션 종료시 선택한 애니메이션으로 변환
+								 //nIndex = 현재 사용중인 애니메이션 셋
+								 //nOutIndex = 현재 사용중인 애니메이션 종료시 변경하고자 하는 애니메이션 셋
 	bool SetNextAniMation(int nIndex, int nNextIndex);
+	bool AnimationClose(int nIndex);
+
+	void Load(char* szFolder, char* szFilename);
+	LPD3DXEFFECT LoadEffect(char* szFilename);
+	void Update(ST_BONE* pCurrent, D3DXMATRIXA16* pmatParent);
+	void Render(ST_BONE* pBone = NULL);
+	void SetupBoneMatrixPtrs(ST_BONE* pBone);
+	void Destroy();
+private:
 };
 
